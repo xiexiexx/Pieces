fdb7c268d6471f9e82bebd6090b01dd5b97a6540
Xie Xie <DSAD2015@163.com>
2016-11-02 12:32:44 +0800

    Update with the prefix sum

--- Fano_Coding.cpp
+++ Fano_Coding.cpp
@@ -8,16 +8,15 @@
 // 读者可将本程序改写为类.
 std::vector<double> P;			// 概率向量.
 std::vector<std::string> C;		// 编码向量, 具体使用时C要比P多一个元素用于哨兵.
+std::vector<double> SUM;		// 概率部分和, SUM[i]存放[0, i)区间的概率和, 长度是P.size() + 1.
 
 // 划分的下标范围是[L, R), 我们需要将该区间划分成[L, S), [S, R), 使得差值最小.
 // 调用此函数的要求是区间非空, 也就是L < R. 
 size_t partiton(size_t L, size_t R)
 {
 	size_t S = L;		// 划分初始设定.
-	// R_L为左右差值, 定义为右半部分减去左半部分. 以下是初值设定.
-	double R_L = 0;		
-	for (size_t i = L; i < R; ++i)
-		R_L += P[i];
+	// R_L为左右差值, 定义为右半部分减去左半部分, 直接从预先算好的部分和中提取而不需要重复再算.
+	double R_L = SUM[R]- SUM[L];		
 	double delta = fabs(R_L);
 	for (size_t d = L + 1; d < R + 1; ++d)
 	{
@@ -56,6 +55,11 @@ void example()
 	// C中多预留一个哨兵值, 以保证不会出现超出边界的赋值错误, 这样亦可解决空概率向量的问题
 	C.resize(P.size() + 1);
 	C[0] = "";		// 编码初值为空字符串.
+	// 提前计算部分和以备后续编码之需.
+	SUM.resize(P.size() + 1);
+	SUM[0] = 0;
+	for (size_t i = 1; i < SUM.size(); ++i)
+		SUM[i] = SUM[i - 1] + P[i - 1];
 	Fano_Coding(0, P.size());
 	C.pop_back();	// 将哨兵去除.
 	for (size_t i = 0; i < C.size(); ++i)
