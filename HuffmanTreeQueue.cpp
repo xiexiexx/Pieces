eed1e2c608368d13ca97fb345cc4cfcc92e1596f
Xie Xie <DSAD2015@163.com>
2016-11-10 17:45:18 +0800

    Update HuffmanTreeQueue.cpp

--- HuffmanTreeQueue.cpp
+++ HuffmanTreeQueue.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <queue>
+#include <cmath>
 
 void HuffmanTree::CodingAll(HuffmanNode* p, const std::string& prefix)
 {
@@ -18,11 +19,70 @@ void HuffmanTree::CodingAll(HuffmanNode* p, const std::string& prefix)
 	}
 }
 
+// 基于队列方式的实现, 注意利用哨兵进行了改进.
 void HuffmanTree::HuffmanCoding(std::vector<double>& P, std::vector<std::string>& C)
 {
 	// 后续处理可保证P.size() > 1, 否则不进行编码直接返回.
 	if (P.size() <= 1)
-		return;		
+		return;
+	// 编码向量HC的长度设置为概率向量的长度.
+	HC.resize(P.size());
+	// 对概率向量进行排序.
+	sort(P.begin(), P.end());
+	// 一次性给出所有在编码中会出现的结点, 注意多加了一个哨兵结点.
+	std::vector<HuffmanNode> data(2 * P.size());
+	std::queue<HuffmanNode*> QNODE;	// 待编码的结点指针队列.
+	std::queue<HuffmanNode*> QTREE;	// 编码过程中形成的树指针队列.
+
+	// index作为编号, 指示当前会使用data[index]处的结点.
+	size_t index;		// 注意其作用域不仅限于下面的for循环.
+	// 初始放入N棵仅有根结点的树.
+	for (index = 0; index < P.size(); index++)
+	{
+		// 对应需编码的符号, 编号为自然数.
+		data[index] = { P[index], index, NULL, NULL };
+		// 放入队列QNODE中.
+		QNODE.push(&data[index]);
+	}
+	// 设置哨兵, 其权值为2, 大于概率值总和1.
+	data[index] = { 2, index, NULL, NULL };
+	QNODE.push(&data[index++]);
+	// 循环处理以构建Huffman树, 两个队列中只剩下哨兵和最后的Huffman树时终止.
+	while (QNODE.size() + QTREE.size() > 2)
+	{
+		// 在两个队列中取出依次取出最小元作为左右孩子, 优先处理QNODE中元素以保证码长方差最小.
+		// 由于QNODE中有哨兵, 因此QNODE在循环过程中永远非空.
+		HuffmanNode* Childs[2];	// 左右孩子指针.
+		for (size_t i = 0; i < 2; ++i)
+		{
+			Childs[i] = QNODE.front();
+			// 若QTREE队首权值更小则更新, 否则取QNODE队首元素.
+			if (!QTREE.empty() && QTREE.front()->weight < Childs[i]->weight)
+			{
+				Childs[i] = QTREE.front();
+				QTREE.pop();
+			}
+			else
+				QNODE.pop();
+		}
+		// 处理新结点数据, 编号统一为HC.size(). index更新, 下次使用新结点.
+		data[index] = { Childs[0]->weight + Childs[1]->weight, HC.size(), Childs[0], Childs[1] };
+		HuffmanNode* p = &data[index++];
+		// 将新树放入队列QTREE中.
+		QTREE.push(p);
+	}
+	// 根据QTREE中所剩的唯一Huffman树进行编码.
+	CodingAll(QTREE.front(), "");
+	C = HC;		// 将最终编码复制给C
+}
+
+// 未使用哨兵的版本, 处理判断部分的代码不够简洁.
+/*
+void HuffmanTree::HuffmanCoding(std::vector<double>& P, std::vector<std::string>& C)
+{
+	// 后续处理可保证P.size() > 1, 否则不进行编码直接返回.
+	if (P.size() <= 1)
+		return;
 	// 编码向量HC的长度设置为概率向量的长度.
 	HC.resize(P.size());
 	// 对概率向量进行排序.
@@ -31,10 +91,10 @@ void HuffmanTree::HuffmanCoding(std::vector<double>& P, std::vector<std::string>
 	std::vector<HuffmanNode> data(2 * P.size() - 1);
 	std::queue<HuffmanNode*> QNODE;	// 待编码的结点指针队列.
 	std::queue<HuffmanNode*> QTREE;	// 编码过程中形成的树指针队列.
-	
+
 	// index作为编号, 指示当前会使用data[index]处的结点.
 	size_t index;		// 注意其作用域不仅限于下面的for循环.
-	// 初始放入n棵仅有根结点的树.
+	// 初始放入N棵仅有根结点的树.
 	for (index = 0; index < P.size(); index++)
 	{
 		// 对应需编码的符号, 编号为自然数.
@@ -59,7 +119,7 @@ void HuffmanTree::HuffmanCoding(std::vector<double>& P, std::vector<std::string>
 				}
 				else
 					QNODE.pop();
-			} 
+			}
 			else
 			{
 				// QNODE中无元素, 直接取QTREE中队首元素.
@@ -78,3 +138,4 @@ void HuffmanTree::HuffmanCoding(std::vector<double>& P, std::vector<std::string>
 	CodingAll(QTREE.front(), "");
 	C = HC;		// 将最终编码复制给C
 }
+*/
